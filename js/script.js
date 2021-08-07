80f6211c1f5367434efe30ee732374ac171a1073
Maksym <maksym.motko@gmail.com>
2021-08-08 00:03:23 +0200

    2lesson

--- js/script.js
+++ js/script.js
@@ -10,12 +10,28 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt('One of last watched film?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('One of last watched film?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('One of last watched film?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Little films you watched');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('You are good viewer');
+} else if (personalMovieDB.count >= 30) {
+    console.log('You are kinoman');
+} else {
+    console.log('error happened');
+}
 
 console.log(personalMovieDB);
