4c0c062cf35e77eeaa8ef05313d19e53c00df3bf
Maksym <maksym.motko@gmail.com>
2021-08-12 21:24:09 +0200

    4lesson

--- js/script.js
+++ js/script.js
@@ -1,79 +1,78 @@
 "use strict";
 
-let numberOfFilms;
-
-function start () {
-    numberOfFilms = +prompt('How much films you watched?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('How much films you watched?', '');
-    }
-}
-
-start();
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    private: false
+    private: false,
+    start: function () {
+        personalMovieDB.count = +prompt('How much films you watched?', '');
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('How much films you watched?', '');
+        }
+    },
+
+    rememberMyFilms: () => {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('One of last watched film?', ''),
+                  b = prompt('На сколько оцените его?', '');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+
+    detectPersonalLevel: () => {
+        if (personalMovieDB.count < 10) {
+            console.log('Little films you watched');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('You are good viewer');
+        } else if (personalMovieDB.count >= 30) {
+            console.log('You are kinoman');
+        } else {
+            console.log('error happened');
+        }
+    },
+
+    showMyDB: () => {
+        if (personalMovieDB.private == false) {
+            console.log(personalMovieDB);
+        } else {
+            console.log('Data base is private');
+        }
+    },
+
+    toggleVisibleMyDatabase: () => {
+        if (personalMovieDB.private) {
+            personalMovieDB.private = false;
+        } else {
+            personalMovieDB.private = true;
+        }
+    },
+
+    writeYourGenres () {
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt(`Your favorite genre number ${i}`);
+
+            if (genre === '' || genre == null) {
+                console.log('Вы ввели некоректные данные');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+        }
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
+    }
+
 };
-
-
-function rememberMyFilms () {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('One of last watched film?', ''),
-              b = prompt('На сколько оцените его?', '');
-    
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
-}
-
-rememberMyFilms();
-
-function writeYourGenres (genre) {
-    for (let i = 1; i <= 3; i++) {
-        const a = prompt(`Your favorite genre number ${i}`);
-    
-        if (a != null && a != '' && a.length < 50) {
-            genre[i - 1] = a;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
-}
-
-writeYourGenres(personalMovieDB.genres);
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Little films you watched');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('You are good viewer');
-    } else if (personalMovieDB.count >= 30) {
-        console.log('You are kinoman');
-    } else {
-        console.log('error happened');
-    }
-}
-
-detectPersonalLevel();
-
-function showMyDB() {
-    if (personalMovieDB.private == false) {
-        console.log(personalMovieDB);
-    } else {
-        console.log('Data base is private');
-    }
-}
-
-showMyDB();
